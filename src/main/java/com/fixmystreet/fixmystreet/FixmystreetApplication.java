d66e512deed10f84e561948a9701e603799aa2ac
atircio <atmatias12@gmail.com>
2025-05-22 12:05:34 +0100

    first commit

---
+++ src/main/java/com/fixmystreet/fixmystreet/FixmystreetApplication.java
@@ -0,0 +1,15 @@
+package com.fixmystreet.fixmystreet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixmystreetApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(FixmystreetApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/fixmystreet/fixmystreet/FixmystreetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fixmystreet.fixmystreet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixmystreetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
